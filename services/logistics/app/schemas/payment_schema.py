357bdbc0f8a20766ff5f45fa69758e2b6ee9a9d1
Keshana <alphadew321@gmail.com>
2026-07-26 23:32:16 +0530

    feat: add PaymentCheckoutRequest and PaymentCheckoutResponse models for payment processing

---
+++ services/logistics/app/schemas/payment_schema.py
@@ -0,0 +1,17 @@
+#meke thiyenne bn ara routes_payements wala endpoint ekedi use una request saha reponse wala data thiyena type gana
+
+import uuid
+from pydantic import BaseModel
+
+class PaymentCheckoutRequest(BaseModel):
+    category_id: uuid.UUID
+    amount: float
+    quantity: int
+
+class PaymentCheckoutResponse(BaseModel):
+    order_id: str
+    merchant_id: str
+    hash: str
+    amount: float
+    currency: str
+    items: str       
