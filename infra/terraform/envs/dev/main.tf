49696dbed7e978aaa8ed2235a341811059485d49
Chandupa <chandupadilhan04@gmail.com>
2026-08-20 16:59:39 +0530

    change terraform main.tf

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -32,10 +32,11 @@ module "registry" {
 }
 
 module "runtime" {
-  source  = "../../modules/app-runtime"
-  env     = "dev"
-  vpc_id  = module.network.vpc_id
-  subnets = module.network.private_subnets
+  source         = "../../modules/app-runtime"
+  env            = "dev"
+  vpc_id         = module.network.vpc_id
+  subnets        = module.network.private_subnets
+  public_subnets = module.network.public_subnets
 
   services = {
     iam       = { image = "${module.registry.urls["iam"]}:${var.release}", port = 8000, cpu = 256, memory = 512, min = 1, max = 1, env = { DATABASE_URL = var.manual_db_url, RABBITMQ_URL = module.broker.url }, secrets = { JWT_PRIVATE_KEY = module.secrets.arns["jwt-signing-key"] } }
@@ -43,6 +44,10 @@ module "runtime" {
     rto       = { image = "${module.registry.urls["rto"]}:${var.release}", port = 8080, cpu = 256, memory = 512, min = 1, max = 2, scale_metric = "connections", env = { DATABASE_URL = var.manual_db_url, RABBITMQ_URL = module.broker.url } }
     analytics = { image = "${module.registry.urls["analytics"]}:${var.release}", port = 8000, cpu = 256, memory = 512, min = 1, max = 1, env = { DATABASE_URL = var.manual_db_url, RABBITMQ_URL = module.broker.url } }
     volunteer = { image = "${module.registry.urls["volunteer"]}:${var.release}", port = 8000, cpu = 256, memory = 512, min = 1, max = 1, env = { DATABASE_URL = var.manual_db_url, RABBITMQ_URL = module.broker.url } }
-    gateway   = { image = "${module.registry.urls["gateway"]}:${var.release}", port = 8000, cpu = 256, memory = 512, min = 1, max = 1, env = { ENVIRONMENT = "aws", IAM_URL = "http://iam:8000", LOGISTICS_URL = "http://logistics:8000", ANALYTICS_URL = "http://analytics:8000", VOLUNTEER_URL = "http://volunteer:8000", RTO_URL = "http://rto:8080" } }
+    gateway   = { image = "${module.registry.urls["gateway"]}:${var.release}", port = 8000, cpu = 256, memory = 512, min = 1, max = 1, public = true, env = { ENVIRONMENT = "aws", IAM_URL = "http://iam:8000", LOGISTICS_URL = "http://logistics:8000", ANALYTICS_URL = "http://analytics:8000", VOLUNTEER_URL = "http://volunteer:8000", RTO_URL = "http://rto:8080" } }
   }
+}
+
+output "alb_url" {
+  value = module.runtime.alb_url
 }
